664cde711b7cbd9acb738bb8d3418ea5a74fa62b
Shashi Bhushan Singh <shashibhushan3105@gmail.com>
2024-09-03 00:48:07 +0530

    blogger

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -5,7 +5,7 @@ import appwriteService from "../../appwrite/config";
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-function PostForm() {
+export default function PostForm({ post }) {
   const{register, handleSubmit, watch, setValue, control, getValues} = useForm({
                defaultValues: {
                 title: post?.title || '',
@@ -16,7 +16,7 @@ function PostForm() {
   })
 
   const navigate = useNavigate()
-  const userData = useSelector((state) => state.user.userData)
+  const userData = useSelector((state) => state.auth.userData)
 
   const submit = async (data) => {
     if (post) {
@@ -41,33 +41,32 @@ function PostForm() {
         const dbPost = await appwriteService.createPost({ ...data, userId: userData.$id});
 
         if (dbPost) {
-          navigate(`./post/${dbPost.$id}`);
+          navigate(`/post/${dbPost.$id}`);
         }
       }
      }
-  }
+  };
 
-  const slugTransform = useCallback(() => {
-          if (value && typeof value === 'string')
+  const slugTransform = useCallback((value) => {
+          if (value && typeof value === "string")
             return value
              .trim()
              .toLowerCase()
-             .replace(/^[a-zA-Z\d\s]+/g, '-')
-             .replace(/\s/g, '-')
+             .replace(/[^a-zA-Z\d\s]+/g, "-")
+             .replace(/\s/g, "-");
 
-             return ''
-  }, [])
+             return "";
+  }, []);
 
   React.useEffect (() => {
  const subscription = watch((value, {name}) => {
-     if (name === 'title') {
-      setValue('slug', slugTransform(value.title,{shouldValidate: true}))
+     if (name === "title") {
+      setValue("slug", slugTransform(value.title,{shouldValidate: true}));
      }
- })
-  return () => {
-    subscription.unsubscribe()
-  }
-  }, [watch, slugTransform, setValue])
+ });
+  return () => 
+    subscription.unsubscribe();
+  }, [watch, slugTransform, setValue]);
   return (
     <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
     <div className="w-2/3 px-2">
@@ -116,7 +115,5 @@ function PostForm() {
         </Button>
     </div>
     </form>
-  )
+  );
 }
-
-export default PostForm
